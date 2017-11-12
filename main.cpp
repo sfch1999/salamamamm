b62ed34bdcb72ce73b51d075327c1093c5a92ece
soroosh farghadani <irs4lr@sorooshs-MacBook-Pro.local>
2017-11-12 16:06:43 +0330

    alam

---
+++ main.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    int a,b,c;
+    cout<<"enter n"<<endl;
+    cin>>a>>b>>c;
+    cout<<endl;
+    cin>>
+    //salaaaaaam
+    return 0;
+}
